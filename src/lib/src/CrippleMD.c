9fc6d79518bbabe40683e711d6b012678f3361d9
canyonhhh <jorislisas@gmail.com>
2022-12-10 16:34:39 +0200

    created parsing functions

--- src/lib/src/CrippleMD.c
+++ src/lib/src/CrippleMD.c
@@ -7,9 +7,59 @@ int parse_md(FILE *inFile, FILE *outFile){
     }
 
     int elements_read = BUFFER_SIZE;
+    int escape;
 
     while (elements_read == BUFFER_SIZE){
-	elements_read = fread(buffer, sizeof(char), BUFFER_SIZE, inFile);
+
+	elements_read = read_clean_buffer(inFile, buffer);
+
+	for(int i = 0; i < elements_read; ++i){
+
+	    switch (buffer[i]){				// Galimai reikia parse funkcijas 
+		case '#':
+		    parse_heading(buffer, i);
+		    break;
+		case '\n':
+		    parse_paragraph(buffer, i);
+		    break;
+		case ' ':
+		    parse_line_break(buffer, i);
+		    break;
+		case '1':
+		    parse_ordered_list(buffer, i);
+		    break;
+		case '-':
+		    parse_list(buffer, i);
+		    break;
+		case '<':
+		    parse_link(buffer, i);
+		    break;
+		case '[':
+		    parse_formated_link(buffer, i);	// Markdowne vienoda sintakse ir nuotraukoms ir linkams
+		    parse_image(buffer, i);		// Turbut reikia padaryti kazka kad atpazinti tekste image extensiona
+		    break;
+		case '_':
+		    parse_bold(buffer, i);
+		    break;
+		case '*':
+		    parse_italic(buffer, i);
+		    break;
+		case '\\':
+		    ++i;			// Escape char, kad galetum deti # ir kitus special chars
+		default:
+		    fprintf(outFile, "%c", buffer[i]);
+		    break;
+	    }
+	}
+    }
+
+    free(buffer);
+
+    return 0;
+}
+
+int read_clean_buffer(FILE *inFile, char *buffer){
+	int elements_read = fread(buffer, sizeof(char), BUFFER_SIZE, inFile);
 
 	if (*(buffer + BUFFER_SIZE - 1) != ' ' || *(buffer + BUFFER_SIZE - 1) != '\n' && elements_read == BUFFER_SIZE){
 	    partial_read_cleanup(inFile, buffer);
@@ -18,14 +68,7 @@ int parse_md(FILE *inFile, FILE *outFile){
 	    *(buffer + elements_read) = '\0';
 	}
 
-	// TODO Literally everything else
-
-	printf("%s", buffer);
-    }
-
-    free(buffer);
-
-    return 0;
+	return elements_read;
 }
 
 void partial_read_cleanup(FILE *inFile, char *buffer){
@@ -41,4 +84,44 @@ void partial_read_cleanup(FILE *inFile, char *buffer){
     
     fseek(inFile, -(offset), SEEK_CUR);				    // Pastumia cursoriu atgal
     return;
+}
+
+int parse_paragraph(char *buffer, int index){
+    return 0;
+}
+
+int parse_line_break(char *buffer, int index){
+    return 0;
+}
+
+int parse_ordered_list(char *buffer, int index){
+    return 0;
+}
+
+int parse_list(char *buffer, int index){
+    return 0;
+}
+
+int parse_heading(char *buffer, int index){
+    return 0;
+}
+
+int parse_link(char *buffer, int index){
+    return 0;
+}
+
+int parse_formated_link(char *buffer, int index){
+    return 0;
+}
+
+int parse_image(char *buffer, int index){
+    return 0;
+}
+
+int parse_bold(char *buffer, int index){
+    return 0;
+}
+
+int parse_italic(char *buffer, int index){
+    return 0;
 }
--- src/lib/src/CrippleMD.h
+++ src/lib/src/CrippleMD.h
@@ -7,4 +7,15 @@
 #define BUFFER_SIZE 100
   
 int parse_md(FILE *inFile, FILE *outFile);
+int read_clean_buffer(FILE *inFIle, char *buffer);
 void partial_read_cleanup(FILE *inFile, char *buffer);
+int parse_heading();
+int parse_paragraph();
+int parse_line_break();
+int parse_ordered_list();
+int parse_list();
+int parse_link();
+int parse_formated_link();
+int parse_image();
+int parse_bold();
+int parse_italic();
