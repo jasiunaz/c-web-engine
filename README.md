4d43d24199f3d2756ab4e577e9e0210cc7884e85
Marijonas <marijoniukas@gmail.com>
2023-01-10 19:11:12 +0200

    adb

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # c-web-engine
 
 ## Roles
+<<<<<<< HEAD
 - Architektas
 - Builderis
 - Critic
 - Dizaineris
 - Exhibitorius
 - Facilitatorius
+=======
+
+### Architects & Builders
+Edgaras Akinis, Joris Lisas
+
+### Critic & Designer
+Matas Jasiūnas
+
+### Exhibitor & Facilitator
+Marijonas Grenda
+>>>>>>> 1b4e243fc43dd6e00be07cbc1f2bb12c70c3eab5
---
+++ src/lib/src/CrippleMD.h
@@ -0,0 +1,38 @@
+#ifndef CRIPPLE_MD_H
+#define CRIPPLE_MD_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1000
+#define BLOCK_COUNT 4
+
+enum block_indexes {italic, bold, heading, paragraph};
+
+
+/*
+ *To track which of the md blocks are open
+ *and store their html tags
+ */
+typedef struct Md_block {
+    int open;
+    char html_open[6];
+    char html_close[6];
+    int level;
+} Md_block;
+
+int parse_md_metadata(FILE *inFile, FILE *outFile);
+int parse_md(FILE *inFile, FILE *outFile);
+
+void parse_bold(Md_block *tracker_blocks, FILE *outFile);
+void parse_italic(Md_block *tracker_blocks, FILE *outFile);
+void parse_heading(char current_char, char last_char, Md_block *tracker_blocks, FILE *outFile);
+void parse_paragraph(Md_block *tracker_blocks, FILE *outFile);
+void end_all_blocks(char current_char, char last_char, Md_block *tracker_blocks, FILE *outFile);
+
+int read_clean_buffer(FILE *inFIle, char *buffer);
+void partial_read_cleanup(FILE *inFile, char *buffer);
+void fill_blocks(Md_block *blocks);
+
+#endif
