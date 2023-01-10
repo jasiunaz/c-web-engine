1b4e243fc43dd6e00be07cbc1f2bb12c70c3eab5
canyonhhh <jorislisas@gmail.com>
2023-01-10 16:31:28 +0200

    de-spaghettied the code

--- src/lib/src/CrippleMD.c
+++ src/lib/src/CrippleMD.c
@@ -2,11 +2,14 @@
 
 int parse_md(FILE *inFile, FILE *outFile){
     char *buffer = calloc(BUFFER_SIZE, sizeof(char));
-    Md_block *tracker_blocks = calloc(sizeof(Md_block), SINGLETON_AMOUNT);
+
+    Md_block *tracker_blocks = calloc(sizeof(Md_block), BLOCK_COUNT);
     if (buffer == NULL || tracker_blocks == NULL){
 	return 1;
     }
+
     fill_blocks(tracker_blocks);
+    enum block_indexes blocks;
 
     int elements_read = BUFFER_SIZE;
     char last_char = '\n';
@@ -17,109 +20,42 @@ int parse_md(FILE *inFile, FILE *outFile){
 	for (int i = 0; i < BUFFER_SIZE; ++i){
 	    char current_char = buffer[i];
 
+	    if (last_char == '\n' && current_char != '#'){
+		parse_paragraph(tracker_blocks, outFile);
+	    }
+
 	    switch (current_char){
 		case '#':
-		    parse_heading(current_char, last_char, tracker_blocks);
+		    parse_heading(current_char, last_char, tracker_blocks, outFile);
+		    break;
+		case ' ':
+		    if (tracker_blocks[heading].level != 0){
+			parse_heading(current_char, last_char, tracker_blocks, outFile);
+		    }
+		    else{
+			fprintf(outFile, " ");
+		    }
 		    break;
 		case '*':
-		    parse_bold(current_char, last_char, tracker_blocks);
+		    parse_bold(tracker_blocks, outFile);
 		    break;
 		case '_':
-		    parse_italic(current_char, last_char, tracker_blocks);
+		    parse_italic(tracker_blocks, outFile);
 		    break;
 		case '\n':
-		    end_all_blocks(current_char, last_char, tracker_blocks);
+		    end_all_blocks(current_char, last_char, tracker_blocks, outFile);
 		    break;
 		case '\\': // Runs through to default and prints next character
 		    ++i;
 		default:
 		    fprintf(outFile, "%c", buffer[i]);
 	    }
+	    last_char = current_char;
 	}
     }
 
     return 0;
 }
-
-/*int parse_md(FILE *inFile, FILE *outFile){*/
-    /*char *buffer = calloc(BUFFER_SIZE, sizeof(char));*/
-    /*Md_block *blocks = calloc(sizeof(Md_block), SINGLETON_AMOUNT);*/
-    /*if (buffer == NULL || blocks == NULL){*/
-	/*return 1;*/
-    /*}*/
-    /*fill_blocks(blocks);*/
-
-    /*int elements_read = BUFFER_SIZE;*/
-    /*char last_char = '\n';*/
-
-    /*while (elements_read == BUFFER_SIZE){*/
-
-	/*elements_read = read_clean_buffer(inFile, buffer);*/
-
-	/*for(int i = 0; i < BUFFER_SIZE && CUR_CHAR != '\0'; ++i){*/
-	    /*if ((CUR_CHAR == '#' && last_char == '\n') || (CUR_CHAR == '#' && last_char == '#') || (blocks[HEADING_INDEX].level != 0)){*/
-		/*blocks[HEADING_INDEX].level += 1;*/
-		/*if (CUR_CHAR == ' '){*/
-		    /*blocks[HEADING_INDEX].level -= 1;*/
-		    /*fprintf(outFile, "<h%d>", blocks[HEADING_INDEX].level);*/
-		    /*blocks[HEADING_INDEX].set = 1; */
-		    /*last_char = CUR_CHAR;*/
-
-		    /*sprintf(blocks[HEADING_INDEX].html_close, "</h%d>", blocks[HEADING_INDEX].level);*/
-		    /*blocks[HEADING_INDEX].level = 0;*/
-		    /*continue;*/
-		/*}*/
-	    /*}*/
-	    /*if (CUR_CHAR != '#' && last_char == '\n'){*/
-		/*fprintf(outFile, "%s", blocks[PARAGRAPH_INDEX].html_open);*/
-		/*blocks[PARAGRAPH_INDEX].set = 1;*/
-		/*last_char = CUR_CHAR;*/
-	    /*}*/
-	    /*if (CUR_CHAR == '*' && blocks[BOLD_INDEX].set == 0){*/
-		/*blocks[BOLD_INDEX].set = 1;*/
-		/*fprintf(outFile, "%s", blocks[BOLD_INDEX].html_open);*/
-		/*last_char = CUR_CHAR;*/
-		/*continue;*/
-	    /*}*/
-	    /*else if (CUR_CHAR == '*' && blocks[BOLD_INDEX].set == 1){*/
-		/*blocks[BOLD_INDEX].set = 0;*/
-		/*fprintf(outFile, "%s", blocks[BOLD_INDEX].html_close);*/
-		/*last_char = CUR_CHAR;*/
-		/*continue;*/
-	    /*}*/
-	    /*if (CUR_CHAR == '_' && blocks[ITALIC_INDEX].set == 0){*/
-		/*blocks[ITALIC_INDEX].set = 1;*/
-		/*fprintf(outFile, "%s", blocks[ITALIC_INDEX].html_open);*/
-		/*last_char = CUR_CHAR;*/
-		/*continue;*/
-	    /*}*/
-	    /*else if (CUR_CHAR == '_' && blocks[ITALIC_INDEX].set == 1){*/
-		/*blocks[ITALIC_INDEX].set = 0;*/
-		/*fprintf(outFile, "%s", blocks[ITALIC_INDEX].html_close);*/
-		/*last_char = CUR_CHAR;*/
-		/*continue;*/
-	    /*}*/
-	    /*if (CUR_CHAR == '\n'){*/
-		/*for(int i = 0; i < SINGLETON_AMOUNT; ++i){*/
-		    /*if (blocks[i].set == 1){*/
-			/*fprintf(outFile, "%s\n", blocks[i].html_close);*/
-			/*blocks[i].level = 0;*/
-			/*blocks[i].set = 0;*/
-		    /*}*/
-		/*}*/
-		/*last_char = CUR_CHAR;*/
-		/*continue;*/
-	    /*}*/
-	    /*if (CUR_CHAR != '#'){*/
-		/*fprintf(outFile, "%c", CUR_CHAR);*/
-	    /*}*/
-	/*}*/
-    /*}*/
-
-    /*free(buffer);*/
-
-    /*return 0;*/
-/*}*/
 
 int read_clean_buffer(FILE *inFile, char *buffer){
 	int elements_read = fread(buffer, sizeof(char), BUFFER_SIZE, inFile);
@@ -148,49 +84,71 @@ void partial_read_cleanup(FILE *inFile, char *buffer){
     return;
 }
 
-void parse_bold(char current_char, char last_char, Md_block *tracker_blocks){
+void parse_bold(Md_block *tracker_blocks, FILE *outFile){
+    enum block_indexes blocks;
 
+    fprintf(outFile, "%s", tracker_blocks[bold].open == 0 ? tracker_blocks[bold].html_open : tracker_blocks[bold].html_close);
+    tracker_blocks[bold].open = !tracker_blocks[bold].open;
 }
 
-void parse_italic(char current_char, char last_char, Md_block *tracker_blocks){
+void parse_italic(Md_block *tracker_blocks, FILE *outFile){
+    enum block_indexes blocks;
 
+    fprintf(outFile, "%s", tracker_blocks[italic].open == 0 ? tracker_blocks[italic].html_open : tracker_blocks[italic].html_close);
+    tracker_blocks[italic].open = !tracker_blocks[italic].open;	    // Uzdaromas blokas
 }
 
-void parse_heading(char current_char, char last_char, Md_block *tracker_blocks){
+void parse_heading(char current_char, char last_char, Md_block *tracker_blocks, FILE *outFile){
+    enum block_indexes blocks;
 
+    if (last_char == '\n' || last_char == '#' || tracker_blocks[heading].level != 0){
+	tracker_blocks[heading].level += 1;
+	if (current_char == ' '){
+	    tracker_blocks[heading].level -= 1;
+	    fprintf(outFile, "<h%d>", tracker_blocks[heading].level);
+	    tracker_blocks[heading].open = 1; 
+
+	    sprintf(tracker_blocks[heading].html_close, "</h%d>", tracker_blocks[heading].level);
+	    tracker_blocks[heading].level = 0;
+	}
+    }
 }
 
-void end_all_blocks(char current_char, char last_char, Md_block *tracker_blocks){
+void parse_paragraph(Md_block *tracker_blocks, FILE *outFile){
+    enum block_indexes blocks;
 
+    fprintf(outFile, "%s", tracker_blocks[paragraph].html_open);
+    tracker_blocks[paragraph].open = 1;
+}
+
+void end_all_blocks(char current_char, char last_char, Md_block *tracker_blocks, FILE *outFile){
+    enum block_indexes blocks;
+
+    for (int i = 0; i < BLOCK_COUNT; ++i){
+	if (tracker_blocks[i].open == 1){
+	    tracker_blocks[i].open = 0;
+	    tracker_blocks[i].level = 0;
+	    fprintf(outFile, "%s", tracker_blocks[i].html_close);
+	}
+    }
+
+    fprintf(outFile, "\n");
 }
 
 void fill_blocks(Md_block *blocks){
+
+    enum block_indexes block;
+
     for(int i = 0; i < 4; ++i){
-	blocks[i].set = 0;
+	blocks[i].open = 0;
 	blocks[i].level = 0;
     }
-    strncpy(blocks[ITALIC_INDEX].html_open, "<em>", 6);
-    strncpy(blocks[ITALIC_INDEX].html_close, "</em>", 6);
-    strncpy(blocks[BOLD_INDEX].html_open, "<b>", 6);
-    strncpy(blocks[BOLD_INDEX].html_close, "</b>", 6);
-    strncpy(blocks[HEADING_INDEX].html_open, "<h%d>", 6);
-    strncpy(blocks[HEADING_INDEX].html_close, "</h%d>", 6);
-    strncpy(blocks[PARAGRAPH_INDEX].html_open, "<p>", 6);
-    strncpy(blocks[PARAGRAPH_INDEX].html_close, "</p>", 6);
+    strncpy(blocks[italic].html_open, "<em>", 6);
+    strncpy(blocks[italic].html_close, "</em>", 6);
+    strncpy(blocks[bold].html_open, "<b>", 6);
+    strncpy(blocks[bold].html_close, "</b>", 6);
+    strncpy(blocks[heading].html_open, "<h%d>", 6);
+    strncpy(blocks[heading].html_close, "</h%d>", 6);
+    strncpy(blocks[paragraph].html_open, "<p>", 6);
+    strncpy(blocks[paragraph].html_close, "</p>", 6);
 }
-
-/*void fill_blocks(Md_block *blocks){*/
-    /*for(int i = 0; i < 4; ++i){*/
-	/*blocks[i].set = 0;*/
-	/*blocks[i].level = 0;*/
-    /*}*/
-    /*strncpy(blocks[ITALIC_INDEX].html_open, "<em>", 6);*/
-    /*strncpy(blocks[ITALIC_INDEX].html_close, "</em>", 6);*/
-    /*strncpy(blocks[BOLD_INDEX].html_open, "<b>", 6);*/
-    /*strncpy(blocks[BOLD_INDEX].html_close, "</b>", 6);*/
-    /*strncpy(blocks[HEADING_INDEX].html_open, "<h\%d>", 6);*/
-    /*strncpy(blocks[HEADING_INDEX].html_close, "", 6);*/
-    /*strncpy(blocks[PARAGRAPH_INDEX].html_open, "<p>", 6);*/
-    /*strncpy(blocks[PARAGRAPH_INDEX].html_close, "</p>", 6);*/
-/*}*/
-
--- src/lib/src/CrippleMD.h
+++ src/lib/src/CrippleMD.h
@@ -5,16 +5,18 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFFER_SIZE 100
-#define SINGLETON_AMOUNT 4
-#define ITALIC_INDEX 0
-#define BOLD_INDEX 1
-#define HEADING_INDEX 2
-#define PARAGRAPH_INDEX 3
-#define CUR_CHAR buffer[i]
+#define BUFFER_SIZE 1000
+#define BLOCK_COUNT 4
 
+enum block_indexes {italic, bold, heading, paragraph};
+
+
+/*
+ *To track which of the md blocks are open
+ *and store their html tags
+ */
 typedef struct Md_block {
-    int set;
+    int open;
     char html_open[6];
     char html_close[6];
     int level;
@@ -23,10 +25,11 @@ typedef struct Md_block {
 int parse_md_metadata(FILE *inFile, FILE *outFile);
 int parse_md(FILE *inFile, FILE *outFile);
 
-void parse_bold(char current_char, char last_char, Md_block *tracker_blocks);
-void parse_italic(char current_char, char last_char, Md_block *tracker_blocks);
-void parse_heading(char current_char, char last_char, Md_block *tracker_blocks);
-void end_all_blocks(char current_char, char last_char, Md_block *tracker_blocks);
+void parse_bold(Md_block *tracker_blocks, FILE *outFile);
+void parse_italic(Md_block *tracker_blocks, FILE *outFile);
+void parse_heading(char current_char, char last_char, Md_block *tracker_blocks, FILE *outFile);
+void parse_paragraph(Md_block *tracker_blocks, FILE *outFile);
+void end_all_blocks(char current_char, char last_char, Md_block *tracker_blocks, FILE *outFile);
 
 int read_clean_buffer(FILE *inFIle, char *buffer);
 void partial_read_cleanup(FILE *inFile, char *buffer);
